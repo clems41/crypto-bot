08c59b69e8039981ec3bf71e300a5eaa009fea3c
Clement NIOT <cniot@isi.nc>
2022-01-22 18:22:48 +1100

    feat(localRepo): remove useless csv file

--- internal/repository/localRepository/constant.go
+++ internal/repository/localRepository/constant.go
@@ -1,12 +1,6 @@
 package localRepository
 
 const (
-	csvFileNamePosition = "positions.csv"
-	csvFileNamePrice    = "prices.csv"
-	csvFileNameBalance  = "balances.csv"
-)
-
-const (
 	googleSpreadsheetID = "1gx6YYgz0quAw4X4MsrUW9gpJpn-UcNc47Evy1tnx43o"
 	positionRangeUpdate = "Positions!A2:Z1000"
 	priceRangeUpdate    = "Prices!A2:Z10000"
